5cca1de8ed83a1bb52a5bc64ffaa09e6686641c6
vvb2060 <vvb2060@gmail.com>
2020-12-14 23:53:08 +0800

    Restore temporary solution makes the problem visible

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := vvb2060
 LOCAL_CFLAGS    := -Oz
-LOCAL_SRC_FILES := vvb2060.c cpp.cpp
+LOCAL_SRC_FILES := vvb2060.c
 LOCAL_LDLIBS    := -llog
 LOCAL_STATIC_LIBRARIES := xposed_detector
 include $(BUILD_SHARED_LIBRARY)
